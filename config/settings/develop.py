c04781050a89796635ca5afc80c1643ccb7605a7
nanahiryu <ryu2751x7@gmail.com>
2022-03-21 12:12:29 +0900

    settings.pyの分割の本番環境での検証完了

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'django-insecure-)wma!1ac2r9uy#bp%mzf7nwa%4jjx(@xli6a-j&d@vg0r57@9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     # 'default': {
