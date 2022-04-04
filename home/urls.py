310904ee43a0f455f429d2c3d0466c19debddbc6
nanahiryu <ryu2751keio@gmail.com>
2022-04-04 12:13:08 +0900

    create portfolio page

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('portfolio/', views.PortfolioView.as_view(), name='portfolio'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.views import generic
 
 # Create your views here.
+
+
 class IndexView(generic.TemplateView):
     template_name = 'home/index.html'
 
+
 class AboutView(generic.TemplateView):
     template_name = 'home/about.html'
+
+
+class PortfolioView(generic.TemplateView):
+    template_name = 'home/portfolio.html'
