443b6b53a32df126670a61970fd00996e7e02ce1
nanahiryu <ryu2751keio@gmail.com>
2022-04-24 13:25:57 +0900

    navbarのcssを改善しました

--- static/spla/style.js
+++ static/spla/style.js
@@ -1,10 +1,62 @@
-  $(function() {
-    $("ul.menu li").hover(
-      function() {
-        $(".menuSub:not(:animated)", this).slideDown();
+
+  // hoverとactiveをpcとタブレットで切り替える
+$(function () {
+  var userAgent = navigator.userAgent; // ユーザーエージェント判定
+  var link = $('.js-link-hover'); // aタグ要素代入
+  var block = $('.js-btn-hover , .js-div-hover , .js-img-hover'); // buttonタグ、divタグ、imgタグ要素代入
+  var list = $('.js-li-hover'); // liタグ要素代入
+  // aタグを踏んだ時の端末判定とhover装飾
+  if (userAgent.indexOf("iPhone") >= 0 || userAgent.indexOf("iPad") >= 0 || userAgent.indexOf("Android") >= 0) {
+    link.on("touchstart", function () {
+      $(this).addClass("link-hover");
+    });
+    link.on("touchend", function () {
+      $(this).removeClass("link-hover");
+    });
+  } else {
+    link.hover(
+      function () {
+        $(this).addClass("link-hover");
       },
-      function() {
-        $(".menuSub", this).slideUp();
+      function () {
+        $(this).removeClass("link-hover");
       }
     );
-  });
+  }
+  // buttonタグ、divタグ、imgタグを踏んだ時の端末判定とhover装飾
+  if (userAgent.indexOf("iPhone") >= 0 || userAgent.indexOf("iPad") >= 0 || userAgent.indexOf("Android") >= 0) {
+    block.on("touchstart", function () {
+      $(this).addClass("block-hover");
+    });
+    block.on("touchend", function () {
+      $(this).removeClass("block-hover");
+    });
+  } else {
+    block.hover(
+      function () {
+        $(this).addClass("block-hover");
+      },
+      function () {
+        $(this).removeClass("block-hover");
+      }
+    );
+  }
+  // liタグを踏んだ時の端末判定とhover装飾
+  if (userAgent.indexOf("iPhone") >= 0 || userAgent.indexOf("iPad") >= 0 || userAgent.indexOf("Android") >= 0) {
+    list.on("touchstart", function () {
+      $(this).addClass("list-hover");
+    });
+    list.on("touchend", function () {
+      $(this).removeClass("list-hover");
+    });
+  } else {
+    list.hover(
+      function () {
+        $(this).addClass("list-hover");
+      },
+      function () {
+        $(this).removeClass("list-hover");
+      }
+    );
+  }
+});
