d3e878de1e4c94d25f6242d178e23929ea71bd57
nanahiryu <ryu2751keio@gmail.com>
2022-07-28 10:48:47 +0900

    blogのカテゴリーにunique制約をつけました

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,19 @@ from django.db import models
 from mdeditor.fields import MDTextField
 
 # Create your models here.
+
+
 class Category(models.Model):
-    name = models.CharField('タイトル', max_length=255)
+    name = models.CharField('タイトル', max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class DiaryQuerySet(models.QuerySet):
     def published(self):
         return self.filter(created_at__lte=timezone.now)
+
 
 class Diary(models.Model):
     """日記"""
@@ -19,7 +23,8 @@ class Diary(models.Model):
     subtitle = models.CharField('サブタイトル', max_length=256, default="ないしょのはなし")
     # text = models.TextField('本文')
     text = MDTextField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='カテゴリ')
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, verbose_name='カテゴリ')
     created_at = models.DateTimeField('作成日', default=timezone.now)
 
     objects = DiaryQuerySet.as_manager()
